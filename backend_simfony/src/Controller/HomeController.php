f33cac24cbea83c66209e6f0f877d88d3e8c80b0
Jonathan Piboteau <jonathanpiboteau@gmail.com>
2024-06-12 17:57:39 +0200

    Add authentificator

--- backend_simfony/src/Controller/HomeController.php
+++ backend_simfony/src/Controller/HomeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,8 +12,9 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController {
 
     #[Route("/", name: "home")]
-    function index (): Response
+    function index (Request $request): Response
     {
+
         return $this->render('home/index.html.twig', [
             'title' => 'Bonjour les gens !'
         ]);
