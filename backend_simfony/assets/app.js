a0cf88f608e352db54b761402ac2cf63d3607e97
Jonathan Piboteau <jonathanpiboteau@gmail.com>
2024-06-13 14:24:24 +0200

    test asset mapper

--- backend_simfony/assets/app.js
+++ backend_simfony/assets/app.js
@@ -1,10 +1,21 @@
 import './bootstrap.js';
-/*
+
+    /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import CanvasConfetti from 'canvas-confetti';
 
+document.body.addEventListener('click', (event) => {
+    const { clientX, clientY } = event;
+    const confetti = new CanvasConfetti();
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: clientX / window.innerWidth, y: clientY / window.innerHeight },
+    });
+});
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
